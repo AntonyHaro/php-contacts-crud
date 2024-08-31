e6349192c136be3293a0647bf614fcfdaff487e1
AntonyHaro <antonyhpro@outlook.com>
2024-08-31 09:48:45 -0300

    estruturação da lógica de inserir e ler dados

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="form.html">Criar um novo usuário</a>
+
+    <?php
+    require_once "database.php";
+
+    try {
+        $sql = "SELECT * FROM users";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (count($users) > 0) {
+            echo "<table border='1'>";
+            echo "<tr><th>ID</th><th>Username</th><th>Email</th><th>Password</th></tr>";
+            foreach ($users as $user) {
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($user['id']) . "</td>";
+                echo "<td>" . htmlspecialchars($user['username']) . "</td>";
+                echo "<td>" . htmlspecialchars($user['email']) . "</td>";
+                echo "<td>" . htmlspecialchars($user["password"]) . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            ;
+            echo "Nenhum registro encontrado.";
+        }
+
+    } catch (PDOException $e) {
+        echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
+    }
+
+    $conn = null;
+    ?>
+</body>
+
+</html>
